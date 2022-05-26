5e2557d32d9271272e758ea01fa577bef6d0cec8
Clark <daprogrammer1@gmail.com>
2022-05-26 14:08:47 +0800

    switch history to web hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
 	routes,
-	history: createWebHistory()
+	history: createWebHashHistory()
 })
 
 export default router 
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,7 @@
 <template>
-	<div>
-		page not found
+	<div class="flex flex-col items-center gap-3 mt-20">
+		<img src="../assets/emoji-frown-fill.svg" alt="" class="w-[14em] dark:invert">
+		<div class="text-4xl font-medium">404 Page Not Found</div>
+		<div class="text-xl">Sorry, we can't find the page that you're looking for.</div>
 	</div>
 </template>
