3af2b4206445d5cfc7bab7f66906886d6f0f9c55
Clark <daprogrammer1@gmail.com>
2022-06-19 22:08:21 +0800

    Updates

---
+++ assets/Wrapper.4cfa1bf3.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as t,d as n}from"./index.5eb27d52.js";const _={},c={class:"xl1:mx-[300px] tablet:mx-[100px]"};function r(e,a){return s(),t("div",c,[n(e.$slots,"default")])}var p=o(_,[["render",r]]);export{p as _};
