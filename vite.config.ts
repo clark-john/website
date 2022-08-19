7afbd88278521d599b046306eaf7a24bbad3d9e7
Clark <daprogrammer1@gmail.com>
2022-08-19 19:32:22 +0800

    update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
 import { defineConfig } from 'vite';
-import { VitePWA as pwa } from "vite-plugin-pwa";
+import { VitePWA as pwa } from 'vite-plugin-pwa';
 import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
-import manifest from "./manifest.json";
+import manifest from './manifest.json';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [
-		eslint(),
-		react(),
+  plugins: [
+    eslint(),
+    react(),
     pwa({
-      strategies: "injectManifest",
-      srcDir: "",
-      filename: "service-worker.js",
+      strategies: 'injectManifest',
+      srcDir: '',
+      filename: 'service-worker.js',
       manifest,
     }),
-	],
+  ],
   resolve: {
     alias: [
-      { find: "@/", replacement: "/src/" }
+      { find: '@', replacement: path.resolve(__dirname, './src') }
     ]
   }
-})
+});
