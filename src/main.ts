5d38b71e11f72867400cf07bdff6a90cfc8b6a20
clark-john <mjohncl4rk@gmail.com>
2022-11-21 22:23:02 +0800

    integrate sentry

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from "@sentry/tracing";
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn: "https://d186006bd4684af59a6f1ecbb6ff8550@o1363879.ingest.sentry.io/4504197541986304",
+  integrations: [
+    new BrowserTracing({
+      tracePropagationTargets: ["localhost"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0
+});
 
 if (environment.production) {
   enableProdMode();
